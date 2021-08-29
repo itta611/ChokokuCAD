79b612199fa10d1cf9f3b75bed8c0bd21721c9e7
Itta <ittakun611@gmail.com>
2021-08-29 11:40:19 +0900

    編集モードから追加モードに戻るとスナップのチェックが消えたままになるバグを修正

--- js/main.js
+++ js/main.js
@@ -426,7 +426,7 @@ document.querySelector('#chokoku-setting-add-btn').addEventListener('click', fun
   document.querySelector('#chokoku-setting-edit-btn').classList.remove('selected');
   document.querySelector('#chokoku-setting-add-mode-params').classList.remove('hidden');
   document.querySelector('#chokoku-setting .btn').parentNode.classList.remove('hidden');
-  document.querySelector('#chokoku-setting-issnap').parentNode.classList.remove('hidden');
+  document.querySelector('#chokoku-setting-issnap').parentNode.parentNode.classList.remove('hidden');
   this.classList.add('selected');
   nowPath.visible = true;
   statuses['setpath'].change();
