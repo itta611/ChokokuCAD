b4080c79ba93638af57bc94e95f7902ea4d9b590
Itta <86896657+itta611@users.noreply.github.com>
2021-12-14 18:43:03 +0900

    fix bug

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import './domEvents.js';
 import { i18n } from './i18n.js';
 
 if (navigator.userAgent.indexOf('MSIE') !== -1 && navigator.userAgent.indexOf('TRIDENT') !== -1) {
-  alert(i18n('IEブラウザには対応していません。'));
+  alert(i18n('IEブラウザには対応していません。', 'IE browsers are not supported.'));
 }
