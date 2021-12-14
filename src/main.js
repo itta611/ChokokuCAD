a30b1895366a8f9d3c338403a4e6c95c9084dd48
Itta <86896657+itta611@users.noreply.github.com>
2021-12-14 17:49:48 +0900

    move IE detect code to main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 import './renderer.js';
 import './domEvents.js';
+import { i18n } from './i18n.js';
+
+if (navigator.userAgent.indexOf('MSIE') !== -1 && navigator.userAgent.indexOf('TRIDENT') !== -1) {
+  alert(i18n('IEブラウザには対応していません。'));
+}
