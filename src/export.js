a34bcd9740e0563fa7e6a2615a75c7210f31af2c
Itta <86896657+itta611@users.noreply.github.com>
2021-12-26 10:16:15 +0900

    add stl exporter

--- src/export.js
+++ src/export.js
@@ -6,15 +6,27 @@ import 'three/STLExporter';
 export function exportFile(fileName) {
   if (fileName !== '') {
     flagNotSaved(false);
-    let exporter = new THREE.GLTFExporter();
+    let GLTFExporter = new THREE.GLTFExporter();
+    let STLExporter = new THREE.STLExporter();
     let fileContent;
-    exporter.parse(model, function(arg) {
+    let fileFormat = document.querySelector('#export-file-format').options[document.querySelector('#export-file-format').selectedIndex].value;
+    if (fileFormat === 'gltf') {
+      GLTFExporter.parse(model, function(arg) {
+        fileContent = JSON.stringify(arg);
+        let a = document.createElement('a');
+        a.href = 'data:application/octet-stream,' + encodeURIComponent(fileContent);
+        a.download = `${fileName}.gltf`;
+        a.click();
+      });
+    } else if (fileFormat === 'stl') {
+      console.log(STLExporter)
+      let arg = STLExporter.parse(model, { binary: true })
       fileContent = JSON.stringify(arg);
       let a = document.createElement('a');
       a.href = 'data:application/octet-stream,' + encodeURIComponent(fileContent);
-      a.download = `${fileName}.gltf`;
+      a.download = `${fileName}.stl`;
       a.click();
-    });
+    }
   } else {
     alert(i18n('ファイル名を入力してください。', 'Please input the file name.'));
   }
